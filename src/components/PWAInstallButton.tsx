61ac4a9879a04b7b80d7a85a8dc7771f545d8418
KimGrang <kimgrang1202@gmail.com>
2025-09-02 09:16:02 +0900

    update : css 색상대비

--- src/components/PWAInstallButton.tsx
+++ src/components/PWAInstallButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useInstallPrompt } from '../hooks/useInstallPrompt';
+import './PWAInstallButton.css';
 
 const PWAInstallButton: React.FC = () => {
   const { isInstallable, promptInstall, isInstalled } = useInstallPrompt();
